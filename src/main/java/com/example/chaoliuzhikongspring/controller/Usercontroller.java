7ec03e1af481eea198556bf154199e23c81bfd0a
Yuanyuan <15382353962@163.com>
2025-07-10 22:03:29 +0800

    ClzkSpring
    # Conflicts:
    #	src/main/java/com/example/chaoliuzhikongspring/controller/Usercontroller.java
    #	src/main/java/com/example/chaoliuzhikongspring/entity/user_info.java

--- src/main/java/com/example/chaoliuzhikongspring/controller/Usercontroller.java
+++ src/main/java/com/example/chaoliuzhikongspring/controller/Usercontroller.java
@@ -6,6 +6,8 @@ import com.example.chaoliuzhikongspring.service.Userservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api")
 @CrossOrigin
@@ -16,11 +18,25 @@ public class Usercontroller {
 
     @PostMapping("/user")
     public Result adduser(@RequestBody user_info user){
+//      设置随机10位数为用户ID
+        long randomNum = (long) (Math.random() * 10000000000L);
+        String id = String.format("%010d", randomNum);
+        user.setUser_id(id);
         boolean success = userservice.save(user);
         if(success){
             return Result.success();
         }else {
             return Result.error();
         }
+    }
+
+    @PutMapping("/user")
+    public Result updateuser(@RequestBody user_info user){
+        if(user.getUser_id()==null){
+            return Result.error("ID不能为空");
+        }
+        boolean success = userservice.updateById(user);
+        if(success) return Result.success();
+        else return Result.error();
     }
 }
--- src/main/java/com/example/chaoliuzhikongspring/entity/user_info.java
+++ src/main/java/com/example/chaoliuzhikongspring/entity/user_info.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class user_info {
-    private String id;
+    private String user_id;
     private String user_name;
     private String user_phone;
     private String user_email;
